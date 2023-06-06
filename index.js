d008f639689a34a73163272c2f5558a3fb755f2d
mlcoor <luckiopen@outlook.com>
2023-06-06 13:24:45 +0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+import express, { response } from "express";
+
+const persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+const app = new express();
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
