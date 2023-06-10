502b73c0a850ae260b8d68c5fbeef08f699b42d2
mlcoor <luckiopen@outlook.com>
2023-06-10 17:24:37 +0800

    finish part3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express, { response } from 'express';
+import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import Person from './models/persons.js';
--- models/persons.js
+++ models/persons.js
@@ -6,7 +6,7 @@ const url = process.env.MONGODB_URI;
 mongoose.set('strictQuery', false);
 mongoose
   .connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB');
   })
   .catch(error => {
